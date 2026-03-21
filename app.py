3ff4b8be582a7ecc6b782670b87d4a3d1a00042e
agriranesalma <agriranesalma99@gmail.com>
2026-03-21 02:45:20 +0000

    Update app.py

--- app.py
+++ app.py
@@ -453,7 +453,7 @@ def show_stop1_story():
     st.markdown(f"""<div class="magic-card"><h3>🛤️ The Journey Continues</h3><p>{stop["transition"]}</p></div>""", unsafe_allow_html=True)
     st.markdown("### 🎧 Listen to Hassani Poetry")
     st.info("💡 Tip: Use headphones for an immersive experience.")
-    st.image("sahara.png", caption="The Sahara — where poetry travels with the wind", use_container_width=True, width =100)
+    st.image("sahara.png", caption="The Sahara — where poetry travels with the wind", use_container_width=True)
     if st.button("🎧 Play the voice of the desert"):
         audio_file = open("hassani_poetry.mp3", "rb")
         st.audio(audio_file.read(), format="audio/mp3")
@@ -483,7 +483,7 @@ def show_partner_code_gate(next_label, next_stop_num):
     st.image(
         "sahrawijewlery.png",
         caption="A traditional Sahrawi silver piece you will learn to create",
-        use_container_width=True, width =50
+        use_container_width=True
     )
     st.markdown("""
     <div class="magic-card">
@@ -708,7 +708,23 @@ def show_stop9_pottery():
 # ---------------- ROUTING ----------------
 if st.session_state.page == "home":
     st.markdown('<h1 class="big-title">Kenz Quest - مهمة الكنز</h1>', unsafe_allow_html=True)
-    st.markdown('<div class="tag-subtitle">Secrets of the past, creativity of the present. • أسرار الماضي، إبداع الحاضر</div>', unsafe_allow_html=True)
+    st.markdown("""
+<style>
+.mini-tagline {
+    font-size: 12px;
+    color: #c9a96e;
+    font-style: italic;
+    text-align: center;
+    transform: rotate(-2deg);
+    display: inline-block;
+    margin-top: -10px;
+}
+</style>
+
+<div class="mini-tagline">
+Secrets of the past, creativity of the present. • أسرار الماضي، إبداع الحاضر
+</div>
+""", unsafe_allow_html=True)
     st.markdown('<h3 class="section-header">🗺️ Click on a region / اضغط على جهة</h3>', unsafe_allow_html=True)
     try:
         image = Image.open("morocco_regions_map.png")
